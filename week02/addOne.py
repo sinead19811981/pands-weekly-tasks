254a1dfbfa8a33bb0219d21761207c615973d24d
Sinead McCormack <sgmccormac81@gmail.com>
2024-03-01 16:19:51 +0000

    addOne.py

--- week02/addOne.py
+++ week02/addOne.py
@@ -1,5 +1,14 @@
+'''
 number = int(input('please enter a number: '))
 newNumber = number +1
 print (f'{number} plus one is {newNumber}')
 
-           
+'''
+
+name = input("Please enter your name: ")
+print (name)
+age = input ("Whats your age? ")
+print ('Hello ' + name + ' and you are ' + age +' old')
+origin = input ("Where you from? ")
+print ('Hello ' + name + ' from ' + origin + ' and you are ' + age + ' years old')
+        
