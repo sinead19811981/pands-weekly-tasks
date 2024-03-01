eaf4be883730abe485254ddda4c3751b66b685ea
Sinead McCormack <sgmccormac81@gmail.com>
2024-03-01 16:51:29 +0000

    bank.py

---
+++ week02/bank.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+print ('Hello ' + name)
+amount1 = int(input("Please enter an amount of money in cents "))
+amount2 = int(input("Thank you and please enter another amount in cents "))
+print (amount1 + amount2)
