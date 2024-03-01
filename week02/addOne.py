d0012a476db0ef7d666746906de376609e8804cb
Sinead McCormack <sgmccormac81@gmail.com>
2024-03-01 15:54:45 +0000

    week02

---
+++ week02/addOne.py
@@ -0,0 +1,5 @@
+number = int(input('please enter a number: '))
+newNumber = number +1
+print (f'{number} plus one is {newNumber}')
+
+           
---
+++ week02/multiply.py
@@ -0,0 +1,23 @@
+# Simple Math program
+# Sinead McCormack
+
+'''
+answer = 111 *555
+print (answer)
+
+'''
+'''
+name = input ("What is your name? ")
+print(name)
+Age = input("how old are you? ")
+print("Thanks") 
+
+'''
+
+name = input(" Enter your name ") 
+print('Hello ' + name + '\nNice to meet you') 
+age = input("How old are you? " )
+print ('Thank you ' + name )
+
+
+
