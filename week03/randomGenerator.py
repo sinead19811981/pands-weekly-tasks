f00589ee68b7ec4b3f5945e5377cfdbeef66eb2a
Sinead McCormack <sgmccormac81@gmail.com>
2024-03-05 15:35:28 +0000

    week03

--- week03/randomGenerator.py
+++ week03/randomGenerator.py
@@ -1,16 +1,28 @@
 # program that prints out a random number between 1 and 10
+#A uthor Sinead McCormack
 
 import random
 
 number = random.randint(1, 10)
 print("here is the ramdom number: {} ".format(number))
 
+# program to generate random number from useer inputting a range 
+
+min_Num = int(input ("Enter a min number from 1 to 10: "))
+max_Num = int(input ("Enter a max number from 1 to 10: "))
+print(random.randrange(min_Num, max_Num))
 
 
-x = int(input("Enter the number of Random no. Display = "))
-start_Val = int("Enter the first value = ")
-end_Val = int("Enter the last value = ")
-for i in range (0, x):
-print(random.randint(start_Val, end_Val))
+  
+    
+
+ 
+   
+
+
+
+
+
+
 
 
