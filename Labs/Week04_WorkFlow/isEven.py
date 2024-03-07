e226435c9b660fbef1a8091918d19bdf6f5e319c
Sinead McCormack <sgmccormac81@gmail.com>
2024-03-07 12:35:27 +0000

    Week03_Workflow

---
+++ Labs/Week04_WorkFlow/isEven.py
@@ -0,0 +1,10 @@
+# program to determin if user input is odd or even
+# Author Sinead McCormack
+
+number = int (input( "Enter an integer: "))
+
+if (number % 2) ==0:
+    print (f"{number} is an even number")
+
+else:
+    print (f"{number} is an odd number")
