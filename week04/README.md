eef1ccd090d7f9ace5fe37ee8fac676db9ac065c
Sinead McCormack <sgmccormac81@gmail.com>
2024-03-08 13:09:09 +0000

    Week04

---
+++ week04/README.md
@@ -0,0 +1,9 @@
+1. Start with any positive integer . 
+
+2. Then each term is obtained from the previous term as follows: 
+  1. If the previous term is even, the next term is one half of the previous term. 
+  2. If the previous term is odd, the next term is 3 times the previous term plus 1. 
+  3. The conjecture is that no matter what value of n, the sequence will always reach 1.
+  a. If the previous term is even, the next term is one half of the previous term. 
+  b. If the previous term is odd, the next term is 3 times the previous term plus 1. 
+  c. The conjecture is that no matter what value of n, the sequence will always reach 1.
---
+++ week04/collatz.py
@@ -0,0 +1,50 @@
+# the user inputs any positive integer 
+# and outputs the successive values of the following calculation.
+
+# The Collatz conjecture is a conjecture in mathematics that concerns sequences defined as follows: start with any positive integer n. Then each term is obtained from the previous term as follows:
+
+# If the previous term is even,the next term is half of the previous term.
+# If the previous term is odd,the next term is 3 times the previous term plus 1.
+# The conjecture is that no matter what value of n, the sequence will always reach 1.
+
+# if x is odd,then next term 3*x + 1
+# if x is even,then next term x//2
+
+# Author Sinead McCormack
+
+
+def collatz (number):
+    if number % 2 ==0:
+        return number //2
+    elif number % 2 ==1:
+        return 3 * number +1
+    
+number = int(input("Please enter a positive Number: "))
+
+while number !=1:
+    collatz(number)
+    number = collatz(number)
+    print(number)
+  
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
